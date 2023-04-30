7da866cd298889120a9430a923ad33ded0e9fdf3
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2023-04-30 15:31:36 +0300

    refactor: add project config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './main.scss';
+
+function component(text) {
+    const element = document.createElement('h1');
+    element.textContent = text;
+    return element;
+  }
+
+  document.body.prepend(component('Проект собран на Webpack'));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    filename: 'script/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+          filename: path.join('icons', '[name].[contenthash][ext]'),
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'template.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].css',
+    }),
+  ],
+};
